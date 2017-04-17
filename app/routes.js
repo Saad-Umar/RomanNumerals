ebd4e789f94d3b7dbd6b1d4207b9fc1613c17e91
Saad Umar <SUmar-WS@Saads-MacBook.local>
2017-04-17 21:18:35 +0500

    Done - Not optimised

--- app/routes.js
+++ app/routes.js
@@ -1,41 +1,52 @@
 var express = require('express');
 
 module.exports = function(app) {
-
+    var numberToRoman = {"1":"I","4":"IV","5":"V","9":"IX","10":"X","40":"XL","50":"L","90":"XC","100":"C","400":"CD","500":"D","900":"CM","1000":"M","4000":"MMMM"};
+    app.get('/')
     app.get('/convert/:number', function (req,res) {
             if (!req.params.number) {
                 return res.status(400).send('Invalid parameters');
             } else {
-                var fixedNumerals = ["I","X","C","M","IV","XL","CD","V","L","D","IX","XC","CM"];
                 var numberArray = req.params.number.split('');
                 var romanString = "";
                 for (var i = 0; i < numberArray.length; i++) {
-                    var repetitiveNumeral = "";
-                    var offset = (numberArray.length - 1) - i;
+                    //var repetitiveNumeral = "0";
                     if (numberArray[i]==0) {
                         continue;
-                    } else if (numberArray[i] == 1) {
-                        offset += 0;
-                    } else if (numberArray[i] == 4) {
-                        offset += 4;
-                    } else if (numberArray[i] == 5)  {
-                        offset +=  7;
-                    } else if (numberArray[i] == 9) {
-                        offset += 10;
-                    } else {
-                        var numberOfRepetition = numberArray[i]%5 - 1;
-                        if (numberArray[i] < 5) {
-                            offset = 0;
-                        } else {
-                            offset = 7;
-                            numberOfRepetition = numberArray[i]%5;
-                        }
-                        for (var i=0;i<numberOfRepetition;i++) {
-                            repetitiveNumeral += "I";
-
-                        }
+                    } else if (numberArray[i]==1||numberArray[i]==4||numberArray[i]==5||numberArray[i]==9) {
+                       var fixedNumeral = numberArray[i];
+                        for (var j = 0; j < numberArray.length - 1 - i;j++) {
+                             fixedNumeral += "0";
                     }
-                    romanString += fixedNumerals[offset] + repetitiveNumeral;
+                    romanString += numberToRoman[fixedNumeral];
+            
+                    } else if (numberArray[i]<5) {
+                       var repetitiveNumeral = 1;
+                        for (var j = 0; j < numberArray.length - 1 - i;j++) {
+                             repetitiveNumeral += "0";
+                    }
+                    for (var j = 0; j < numberArray[i];j++) {
+                        romanString += numberToRoman[repetitiveNumeral];
+                    }
+                    
+                    } else {
+                       var repetitiveNumeral = 5;
+                        for (var j = 0; j < numberArray.length - 1 - i;j++) {
+                             repetitiveNumeral += "0";
+                        }
+                       romanString += numberToRoman[repetitiveNumeral];
+                      
+                       for (var j = 0; j < numberArray[i] - 5;j++) {
+                             repetitiveNumeral = 1;
+                              for (var k = 0; k < numberArray[i] - 5;k++) {
+                             repetitiveNumeral += "0";
+                            
+                        }
+                       romanString += numberToRoman[repetitiveNumeral];
+                    }
+                    
+                    }
+            
                 }
                 return res.status(200).send(romanString);
             }
